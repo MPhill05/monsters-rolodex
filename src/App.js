76fab9cb2f78b45d5f2dad712cfdc8b74fc65574
Mark <mphill05@gmail.com>
2020-08-19 10:03:16 -0700

    basic funcationality

--- src/App.js
+++ src/App.js
@@ -1,29 +1,39 @@
 import React, { Component } from 'react';
+
+import { CardList } from './components/card-list/card-list.component';
+
 import './App.css';
-import { render } from '@testing-library/react';
 
 class App extends Component {
   constructor() {
     super();
 
     this.state = {
-      monsters: []
+      monsters: [],
+      searchField: ''
     };
   }
 
   componentDidMount() {
     fetch('https://jsonplaceholder.typicode.com/users')
-    .then(response => response.json())
-    .then(users => this.setState({ monsters : users }))
+      .then(response => response.json())
+      .then(users => this.setState({ monsters: users }))
   }
 
   render() {
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter(monster =>
+        monster.name.toLowerCase().includes(searchField.toLowerCase())
+      )
     return (
       <div className='App'>
-        { this.state.monsters.map(monster =>
-          <h1 key={monster.id}> { monster.name } </h1>)}
+        <input type='search'
+          placeholder='search monsters'
+          onChange={e => this.setState({ searchField: e.target.value })}
+        />
+        <CardList monsters={filteredMonsters} />
       </div>
-    )
+    );
   }
 }
 
